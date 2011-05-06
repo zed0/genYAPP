cbddc755582000ab3ea696268f90ee5328357c06
Ben Falconer <ben@falconers.me.uk>
2011-05-06 10:13:25 +0100

    added uploader script

---
+++ YAPP.php
@@ -0,0 +1,47 @@
+<?php
+
+$uploadDir = 'xlsx/';
+$destDir = 'yapps/';
+$destName = 'yappsFor' . date('dmy') . '.zip';
+$tempDir = 'temp/';
+
+?>
+<html>
+	<head>
+	</head>
+	<body>
+
+<?php
+
+if($_FILES['uploadedFile'])
+{
+	$targetPath = './' . $uploadDir . basename($_FILES['uploadedFile']['name']);
+
+	if(move_uploaded_file($_FILES['uploadedFile']['tmp_name'], $targetPath)) {
+		echo('The file ' .  basename($_FILES['uploadedFile']['name']) . ' has been uploaded<br />');
+		echo('Converting...<br />');
+		chdir('./' . $tempDir);
+		exec('../genYAPP.php ' . '.' . $targetPath);
+		exec('zip -r ../' . $destDir . $destName . ' .');
+		chmod('../' . $destDir . $destName, 0755);
+		echo('<a href="./' . $destDir . $destName . '" >Download</a><br />');
+	}
+	else
+	{
+		echo("There was an error uploading the file, please try again!");
+	}
+}
+else
+{
+?>
+	<form enctype="multipart/form-data" action="YAPP.php" method="POST">
+		<input type="hidden" name="MAX_FILE_SIZE" value="100000" />
+		Choose a .csv file to upload: <input name="uploadedFile" type="file" /><br />
+		<input type="submit" value="Upload File" />
+	</form>
+<?php
+}
+
+?>
+	</body>
+</html>
