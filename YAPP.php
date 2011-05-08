373c3b4e12e5c12d08b77fdf7cd76041aad44759
Ben Falconer <ben@falconers.me.uk>
2011-05-08 15:38:05 +0100

    Cleaned up after script as run preventing extraneous yapp files being in the resulting zip file.

--- YAPP.php
+++ YAPP.php
@@ -20,19 +20,21 @@ if($_FILES['uploadedFile'])
 	if(move_uploaded_file($_FILES['uploadedFile']['tmp_name'], $targetPath)) {
 		echo('The file ' .  basename($_FILES['uploadedFile']['name']) . ' has been uploaded<br />');
 		echo('Converting...<br />');
+		unlink('./'.$destDir.$destName);
+		mkdir($tempDir);
 		chdir('./' . $tempDir);
 		exec('../genYAPP.php ' . '.' . $targetPath);
 		exec('zip -r ../' . $destDir . $destName . ' .');
 		chmod('../' . $destDir . $destName, 0755);
 		echo('<a href="./' . $destDir . $destName . '" >Download</a><br />');
+		chdir('../');
+		exec('rm -rf ' . $tempDir);
 	}
 	else
 	{
 		echo("There was an error uploading the file, please try again!");
 	}
 }
-else
-{
 ?>
 	<form enctype="multipart/form-data" action="YAPP.php" method="POST">
 		<input type="hidden" name="MAX_FILE_SIZE" value="100000" />
@@ -40,7 +42,6 @@ else
 		<input type="submit" value="Upload File" />
 	</form>
 <?php
-}
 
 ?>
 	</body>
